a131299f3f8168f4b883f2576f7d0ac0b6921c45
EamonYin <xiaominglovejava@163.com>
2022-05-10 10:56:58 +0800

    第一次提交

---
+++ src/main/java/com/eamon/designpatternv2/DesignPatternV2Application.java
@@ -0,0 +1,13 @@
+package com.eamon.designpatternv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignPatternV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/eamon/designpatternv2/DesignPatternV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eamon.designpatternv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
