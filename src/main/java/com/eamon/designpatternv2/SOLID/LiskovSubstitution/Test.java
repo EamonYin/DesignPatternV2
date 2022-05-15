5141d78c7f37bbd9dc211e171b77793796195279
EamonYin <xiaominglovejava@163.com>
2022-05-15 20:25:26 +0800

    里氏替换

--- src/main/java/com/eamon/designpatternv2/SOLID/LiskovSubstitution/Test.java
+++ src/main/java/com/eamon/designpatternv2/SOLID/LiskovSubstitution/Test.java
@@ -4,6 +4,7 @@ package com.eamon.designpatternv2.SOLID.LiskovSubstitution;
  * @Author: Eamon
  * @Date: 2022/5/15 18:40
  *
+ * Liskov Substitution(里氏替换原则)
  * 子类可以扩展父类的功能，但不能改变父类原有的功能(只添加新的方法，尽量不要重写父类方法，也尽量不要重载父类方法。)
  * 1. 子类可以实现父类的抽象方法，但不能覆盖父类的非抽象方法。
  * 2. 子类中可以增加自己特有的方法。
